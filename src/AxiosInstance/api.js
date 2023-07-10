1271fba0dad1716fa18c5e69349296b4ebdd79c6
unknown <saqibraza0902@gmail.com>
2023-07-11 01:45:26 +0500

    fix the pagination and call single book api

--- src/AxiosInstance/api.js
+++ src/AxiosInstance/api.js
@@ -1,7 +1,7 @@
-import axios from 'axios'
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://educationserver.vercel.app"
-})
+  baseURL: "http://localhost:5000",
+});
 
-export default api
+export default api;
